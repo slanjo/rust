5d8b1fd0a8dc456f5c40f01a77455c79b97a3b5a
slanjo <slaanjoo@gmail.com>
2021-10-09 13:24:42 +0000

    rust basics

---
+++ mars_calc/target/debug/deps/mars_calc-38ff2d9777917c6d.d
@@ -0,0 +1,5 @@
+/home/admin/Programming/rust/mars_calc/target/debug/deps/mars_calc-38ff2d9777917c6d: src/main.rs
+
+/home/admin/Programming/rust/mars_calc/target/debug/deps/mars_calc-38ff2d9777917c6d.d: src/main.rs
+
+src/main.rs:
