c159959dc4a2b0be40c74f159cc097e089bcfcff
slanjo <slaanjoo@gmail.com>
2023-09-02 16:27:26 +0800

    rust-book

---
+++ branches/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+//  let number = 5;
+
+//  if number %4 == 0  {
+//      println!("number is divisible by 4");
+//  } else if number %3 == 0  {
+//      println!("number is divisible by 3");
+//  } else if number %2 == 0  {
+//      println!("number is divisible by 3");
+//  } else {
+//      println!("number is not divisible by 4, 3, or 2");
+//  }
+    let condition = false;
+    let number = if condition { 5 } else { 6 };
+    println!("the value of the number is {number}");
+
+}
