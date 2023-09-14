b49f73ed745032927ea530999097c36c28240e79
slanjo <slaanjoo@gmail.com>
2023-09-14 20:24:23 +0800

    ownership

--- ownersh/src/main.rs
+++ ownersh/src/main.rs
@@ -10,5 +10,34 @@ fn main() {
     println!("s2 = {s2}");
 //    println!("s = {s}");
 
+    let s = String::from("hello");
+    takes_ownership(s);
+    let x = 5;
+    
+    makes_copy(x);
+    println!("After function return: {x}");
 
+    let s11 = gives_ownership();
+    let s12 = String::from("hello");
+    let s13 = takes_and_gives_back(s12);
+    println!("s11 = {s11}");
+//    println!("s12 = {s12}");
+    println!("s13 = {s13}");
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_int: i32) {
+    println!("{}", some_int);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+   a_string
 }
