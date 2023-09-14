2d952b1a8296f3508ab86f28dcffe5be629dcf65
slanjo <slaanjoo@gmail.com>
2023-09-14 19:37:28 +0800

    ownership

---
+++ ownersh/src/main.rs
@@ -0,0 +1,14 @@
+
+use std::string::String;
+fn main() {
+    let s1 = String::from("Hello");
+    println!("s1 = {s1}");
+//    let s = "M";
+    let s2 = s1.clone();
+    
+    println!("s1 = {s1}");
+    println!("s2 = {s2}");
+//    println!("s = {s}");
+
+
+}
